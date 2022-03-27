5aa1a507a209b11a49d2a529d93919cb888f6d17
Nina-byte <75306778+Nina-byte@users.noreply.github.com>
2022-03-27 20:16:43 +0200

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2021": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
